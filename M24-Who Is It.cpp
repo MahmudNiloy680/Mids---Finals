e1e5f44ca86d6b2afd79658813044d0caa667d74
Neptune <mahmudniloy680work@gmail.com>
2024-11-22 20:34:29 +0600

    Solved M24 Using Array of Objects

--- M24-Who Is It.cpp
+++ M24-Who Is It.cpp
@@ -1,5 +1,56 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main()
+// {
+//     int t;
+//     cin >> t;
+
+//     while (t--)
+//     {
+//         int id[3];  
+//         string name[3];
+//         char section[3];
+//         int marks[3];
+
+//         for (int i = 0; i < 3; i++)
+//         {
+//             cin >> id[i] >> name[i] >> section[i] >> marks[i];
+//         }
+
+
+//         int max_mark = marks[0];
+//         int max_index = 0;
+
+//         for (int i = 1; i < 3; i++)
+//         {
+//             if (marks[i] > max_mark)
+//             {
+//                 max_mark = marks[i];
+//                 max_index = i;
+//             }
+//         }
+
+//         cout << id[max_index] << " " << name[max_index] << " " << section[max_index] << " " << marks[max_index] << endl;
+
+//     }
+//     return 0;
+// }
+
+
+//USING ARRAY OF OBJECT
+
 #include <bits/stdc++.h>
 using namespace std;
+
+class Student
+{
+    public:
+    int id;
+    char name[105];
+    char section;
+    int marks;
+};
 
 int main()
 {
@@ -8,31 +59,22 @@ int main()
 
     while (t--)
     {
-        int id[3];  
-        string name[3];
-        char section[3];
-        int marks[3];
-
+        Student array[3];
         for (int i = 0; i < 3; i++)
         {
-            cin >> id[i] >> name[i] >> section[i] >> marks[i];
+            cin >> array[i].id >> array[i].name >> array[i].section >> array[i].marks;
         }
 
-
-        int max_mark = marks[0];
-        int max_index = 0;
-
-        for (int i = 1; i < 3; i++)
+        Student max_mark = array[0];
+        for (int i = 0; i < 3; i++)
         {
-            if (marks[i] > max_mark)
+            if (array[i].marks > max_mark.marks)
             {
-                max_mark = marks[i];
-                max_index = i;
+                max_mark = array[i];
             }
         }
 
-        cout << id[max_index] << " " << name[max_index] << " " << section[max_index] << " " << marks[max_index] << endl;
-
+        cout << max_mark.id << " " << max_mark.name << " " << max_mark.section << " " << max_mark.marks << endl;
     }
     return 0;
 }
